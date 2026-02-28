139f96d27354c5e80615f4eb8438f02eb4c561db
Ed Dela Cruz <edmundo.delacruz@cartrack.com>
2026-02-28 11:15:39 +0800

    Week 2 sample Code

---
+++ src/main/java/week2Code/NewMainClass.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week2Code;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class NewMainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        imaginaryNumber in = new imaginaryNumber();
+        in.multiple(100, 200);
+        
+        nameOfClass nc = new nameOfClass();
+        nc.multiple(200, 600);
+    }
+    
+}
---
+++ src/main/java/week2Code/nameOfClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week2Code;
+
+/**
+ *
+ * @author Edmundo Dela Cruz
+ */
+public class nameOfClass extends imaginaryNumber{
+    
+ 
+    
+}
